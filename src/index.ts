589e0884e5668ed74be320a08d4173a4826c5883
Sambo Chea <sombochea@cubetiqs.com>
2021-03-10 13:37:21 +0700

    Updated the script and add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const Index = (): void => {
-    console.log("Index", "Hello, you are init the default function!")
-}
+    console.log('Index', 'Hello, you are init the default function!');
+};
 
 export default Index;
